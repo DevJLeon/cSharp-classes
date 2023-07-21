ef9e6696353795f0a83e4db5bfab2819c3cbbef4
Joshua Leon <119650191+DevJLeon@users.noreply.github.com>
2023-07-21 17:16:20 -0500

    Printing Balance Exercise

---
+++ printingBalance.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program{
+
+    static void Main(){
+        decimal[] ammounts = {16305.32m, 18794.16m};
+        Console.WriteLine("  Balance Inicial          Balance final");
+        Console.WriteLine("    {0,-20:C2}{1,14:C2}", ammounts[0],ammounts[1]);
+    }
+}
