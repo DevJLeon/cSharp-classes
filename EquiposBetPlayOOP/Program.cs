b9efb4d43ef8cb39e84aa7a67f137397c4cb4520
DevJLeon <celisjoshleon@gmail.com>
2023-08-01 17:13:36 -0500

    Liga Betplay exercise

---
+++ EquiposBetPlayOOP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int option;
+        do {
+            ShowMenu();
+            option = RequestOption();
+
+            switch(option){
+                case 1:
+                    
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;                    
+            }
+
+        } while(option!=3);
+    }
+
+    static void ShowMenu(){
+        Console.WriteLine("Menú de opciones:");
+        Console.WriteLine("1. Registro Pantel");
+        Console.WriteLine("2. Consulta de datos");
+        Console.WriteLine("3. Salir");
+    }
+        static void MenuRegister(){
+        Console.WriteLine("Menú de opciones:");
+        Console.WriteLine("1. Registro Pantel");
+        Console.WriteLine("2. Consulta de datos");
+        Console.WriteLine("3. Salir");
+    }
+
+    static int RequestOption() {
+        Console.Write("Elige una opcion");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    
+}
+
