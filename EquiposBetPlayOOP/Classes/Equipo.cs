494f6af883e1f57cb5aab4c73a0238b2f57a8beb
DevJLeon <celisjoshleon@gmail.com>
2023-08-03 17:13:13 -0500

    avances Equipo

--- EquiposBetPlayOOP/Classes/Equipo.cs
+++ EquiposBetPlayOOP/Classes/Equipo.cs
@@ -2,5 +2,10 @@ namespace EquiposBetPlayOOP.Classes;
 
 public class Equipo
 {
+    public string Name {get; set;}
+    public List<string> Propietarios {get; set;}
+    public List<Jugador> Jugadores {get; set;}
+    public List <Masajista> Masajistas {get; set;}
+    public List <Entrenador> Entrenadores {get; set;}
     
 }
